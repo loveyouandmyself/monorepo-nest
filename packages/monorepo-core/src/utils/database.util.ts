d3ffdc2cf965dec0483cfe644ccf64821dbeffbd
likun <likun@yinhe.ht>
2021-08-17 09:45:37 +0800

    style: 编译、增加toboolean方法
    affects: @monorepo-interface/core

--- packages/monorepo-core/src/utils/database.util.ts
+++ packages/monorepo-core/src/utils/database.util.ts
@@ -5,6 +5,20 @@ export const getSkip = (page: number, limit: number): number => {
   return Math.max(0, skip);
 };
 
+// 转换为布尔类型
+export const toBoolean = (value: unknown): boolean => {
+  if (typeof value === 'boolean')
+    return value;
+
+  if (typeof value === 'string')
+    return value === 'true' || value === '1';
+
+  if (typeof value === 'number')
+    return value > 0;
+
+  return false;
+};
+
 class IdToStringTransformer implements ValueTransformer {
   to(value: any) {
     return value;
